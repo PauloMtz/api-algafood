1573a229d35a7c35dcf6bb30ddf307fb594af3e9
PauloMtz <pramosmtz@gmail.com>
2023-11-29 21:23:43 -0300

    Criação da aplicação

---
+++ src/test/java/com/algafood/ApiAlgafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algafood;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAlgafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
