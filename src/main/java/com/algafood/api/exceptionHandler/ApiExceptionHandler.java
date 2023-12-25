b42aaff1aec96278a0de5ed84a4dd4a19aabb2c5
PauloMtz <pramosmtz@gmail.com>
2023-12-25 10:00:27 -0300

    Exception global com @ResponseEntityExceptionHandler - até a aula 8.16

--- src/main/java/com/algafood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/algafood/api/exceptionHandler/ApiExceptionHandler.java
@@ -2,58 +2,61 @@ package com.algafood.api.exceptionHandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algafood.domain.exception.EntidadeNaoRemoverException;
 import com.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
-			EntidadeNaoEncontradaException e) {
-		MensagemCustomizada mensagemCustom = MensagemCustomizada.builder()
-			.dataHora(LocalDateTime.now())
-			.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-			.body(mensagemCustom);
+			EntidadeNaoEncontradaException ex, WebRequest request) {
+				
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+			HttpStatus.NOT_FOUND, request);
 	}
 
 	@ExceptionHandler(EntidadeNaoRemoverException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
-			EntidadeNaoRemoverException e) {
-		MensagemCustomizada mensagemCustom = MensagemCustomizada.builder()
-			.dataHora(LocalDateTime.now())
-			.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.CONFLICT)
-			.body(mensagemCustom);
+	public ResponseEntity<?> tratarEntidadeNaoRemoverException(
+			EntidadeNaoRemoverException ex, WebRequest request) {
+
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+			HttpStatus.CONFLICT, request);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-		MensagemCustomizada mensagemCustom = MensagemCustomizada.builder()
-			.dataHora(LocalDateTime.now())
-			.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(mensagemCustom);
+	public ResponseEntity<?> tratarNegocioException(NegocioException ex, 
+		WebRequest request) {
+			
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+			HttpStatus.BAD_REQUEST, request);
 	}
-	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-		MensagemCustomizada mensagemCustom = MensagemCustomizada.builder()
-			.dataHora(LocalDateTime.now())
-			.mensagem("O tipo de mídia não é suportado.").build();
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+			org.springframework.http.HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-			.body(mensagemCustom);
+		if (body == null) {
+			body = MensagemCustomizada.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(status.getReasonPhrase())
+				.build();
+		} else if (body instanceof String) {
+			body = MensagemCustomizada.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem((String) body)
+				.build();
+		}
+
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 }
