bbe739db25a96e4f27771fd101ecd44e5bcd2167
PauloMtz <pramosmtz@gmail.com>
2024-01-01 20:36:52 -0300

    Aula 8.20 - tratando exception de sintaxe

--- src/main/java/com/algafood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/algafood/api/exceptionHandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.algafood.api.exceptionHandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +15,21 @@ import com.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(
+		HttpMessageNotReadableException ex, HttpHeaders headers, 
+		HttpStatus status, WebRequest request) {
+		
+		TypeMessage typeMessage = TypeMessage.RECURSO_NAO_IDENTIFICADO;
+		String detail = "Corpo da requisição inválido. Verifique a sintaxe.";
+
+		CustomMessage customMessage = createCustomMessageBuiler(
+			status, typeMessage, detail).build();
+		
+		return handleExceptionInternal(ex, customMessage, new HttpHeaders(), 
+			status, request);
+	}
     
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> handleEntidadeNaoEncontradaException(
--- src/main/java/com/algafood/api/exceptionHandler/TypeMessage.java
+++ src/main/java/com/algafood/api/exceptionHandler/TypeMessage.java
@@ -10,7 +10,9 @@ public enum TypeMessage {
     RECURSO_NAO_PODE_SER_REMOVIDO("/nao-pode-ser-removido", 
         "Recurso não pode ser removido"),
     RECURSO_INVALIDO("/recurso-invalido", 
-        "Recurso e/ou informação inválidos");
+        "Recurso e/ou informação inválidos"),
+    RECURSO_NAO_IDENTIFICADO("/nao-identificado", 
+        "Recurso não identificado");
 
     private String title;
     private String uri;
