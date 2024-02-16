79fcafa0a4fa060748a93c8bf9d621166208d275
PauloMtz <pramosmtz@gmail.com>
2024-02-16 08:27:04 -0300

    Aula 11.1 - utilizando a anotação @Transactional nas operações no banco de dados

--- src/main/java/com/algafood/domain/service/RestauranteService.java
+++ src/main/java/com/algafood/domain/service/RestauranteService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algafood.domain.exception.EntidadeNaoRemoverException;
 import com.algafood.domain.exception.RestauranteNaoEncontradoException;
@@ -23,6 +24,17 @@ public class RestauranteService {
     @Autowired
     private CozinhaService cozinhaService;
 
+    /*
+     * a anotação transactional serve para manter a consistência das
+     * operações no banco de dados. Exemplo: se dentro do método salvar
+     * tivesse uma outra operação, e uma delas lançasse uma exceção,
+     * isso poderia causar uma inconsistência na operação.
+     * Os métodos do spring já são anotados com transactional, mas
+     * se houver mais de uma operação no método, é melhor anotar.
+     * Inclusive, utilizar o método flush() para descarregar as 
+     * alterações pendentes no banco. O método flush() é da JPA.
+     */
+    @Transactional
     public Restaurante salvar(Restaurante restaurante) {
         Long cozinhaId = restaurante.getCozinha().getId();
 
@@ -33,9 +45,19 @@ public class RestauranteService {
         return restauranteRepository.save(restaurante);
     }
 
+    /*
+     * o método deleteById do spring já é anotado com @Transactional,
+     * isso faz com que ele gerencie as exceções da operação.
+     * Ao anotar esse método excluir, as exceções aqui não são
+     * mais capturadas por este método. Para voltar a capturar, deve-se
+     * forçar a JPA a executar as alterações no banco de dados,
+     * utilizando o método flush() 
+     */
+    @Transactional
     public void excluir(Long restauranteId) {
         try {
             restauranteRepository.deleteById(restauranteId);
+            restauranteRepository.flush();
         } catch (EmptyResultDataAccessException ex) {
             throw new RestauranteNaoEncontradoException(restauranteId);
         } catch (DataIntegrityViolationException ex) {
