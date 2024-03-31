e0cf9388fee01c33412c782610ee9611bca27c99
PauloMtz <pramosmtz@gmail.com>
2024-03-31 16:36:22 -0300

    Aula 13.02 - selecionando campos retornados pela API com JsonFilter do Jackson

--- src/main/java/com/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algafood/api/controller/PedidoController.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,8 @@ import com.algafood.domain.model.Pedido;
 import com.algafood.domain.model.Usuario;
 import com.algafood.domain.repository.PedidoRepository;
 import com.algafood.domain.service.PedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -63,13 +68,34 @@ public class PedidoController {
             throw new NegocioException(e.getMessage(), e);
         }
     }
-    
+
     @GetMapping
+    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+        
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        List<PedidoResumoDto> pedidosDto = pedidoResumoAssembler
+            .convertToCollectionDto(pedidos);
+        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosDto);
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("filtroPedido", SimpleBeanPropertyFilter.serializeAll());
+        //filterProvider.addFilter("filtroPedido", SimpleBeanPropertyFilter.filterOutAllExcept("codigo", "valorTotal"));
+        
+        if (StringUtils.isNotBlank(campos)) {
+            // http://localhost:8080/pedidos?campos=codigo,valorTotal
+            filterProvider.addFilter("filtroPedido", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+        }
+        
+        pedidosWrapper.setFilters(filterProvider);
+
+        return pedidosWrapper;
+    }
+    
+    /*@GetMapping
     public List<PedidoResumoDto> listar() {
         List<Pedido> listaPedidos = pedidoRepository.findAll();
         
         return pedidoResumoAssembler.convertToCollectionDto(listaPedidos);
-    }
+    }*/
     
     @GetMapping("/{codigoPedido}")
     public PedidoDto buscar(@PathVariable String codigoPedido) {
--- src/main/java/com/algafood/api/model/dto/PedidoResumoDto.java
+++ src/main/java/com/algafood/api/model/dto/PedidoResumoDto.java
@@ -3,9 +3,12 @@ package com.algafood.api.model.dto;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonFilter("filtroPedido")
 @Getter
 @Setter
 public class PedidoResumoDto {
