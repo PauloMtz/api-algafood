9b27fe3192732dbe0eb1bc6851fd4398da577663
PauloMtz <pramosmtz@gmail.com>
2024-04-04 20:59:10 -0300

    Aula 13.07 - tratando exceptions URL inválida

--- src/main/java/com/algafood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/algafood/api/exceptionHandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -40,15 +41,30 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@Autowired
 	private MessageSource messageSource;
 
+	// método sobrescrito - digite handleBindException e o ide irá sugerir opções
+	@Override
+	protected ResponseEntity<Object> handleBindException(BindException ex, 
+		HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		// getBindingResult() já utilizado em outros métodos abaixo
+		// é um método de BindException()
+		ex.getBindingResult();
+		
+		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+	}
+
 	@ExceptionHandler({ ValidacaoException.class })
-	public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
+	public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, 
+		WebRequest request) {
+
 		return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(), 
-				HttpStatus.BAD_REQUEST, request);
+			HttpStatus.BAD_REQUEST, request);
 	}
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		HttpHeaders headers, HttpStatus status, WebRequest request) {
+
 	    return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
 	}
 
