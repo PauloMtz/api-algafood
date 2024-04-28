0ab9e6fc54c38156ef8d1109911c1d7a050112bb
PauloMtz <pramosmtz@gmail.com>
2024-04-28 08:17:55 -0300

    Aula 14.04 - validando o tipo de arquivo

--- src/main/java/com/algafood/api/model/inputDto/ProdutoUploadInputDto.java
+++ src/main/java/com/algafood/api/model/inputDto/ProdutoUploadInputDto.java
@@ -3,9 +3,11 @@ package com.algafood.api.model.inputDto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.algafood.core.validation.AnotacaoCriadaFileSize;
+import com.algafood.core.validation.FileContentType;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +18,7 @@ public class ProdutoUploadInputDto {
     
     @NotNull
     @AnotacaoCriadaFileSize(tamanhoMaximo = "5MB")
+    @FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
     private MultipartFile imagem;
 
     @NotBlank
